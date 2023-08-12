62d7a388ec1c52430a27365ffd0df279aaee4580
yichengng <nyicheng12@gmail.com>
2023-08-12 21:21:18 +0800

    Sample design + beautify code

--- StockMarketSite/StockMarketApp/views.py
+++ StockMarketSite/StockMarketApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "StockMarketApp/index.html", {})
+    return render(request, "StockMarketApp/candlestick_chart.html", {})
