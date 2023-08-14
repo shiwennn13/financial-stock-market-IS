02952bc54fdcecc0ae3dc1dd8294c506d64b2a47
yichengng <nyicheng12@gmail.com>
2023-08-14 19:15:25 +0800

    Sample design + beautify code

--- StockMarketSite/StockMarketApp/urls.py
+++ StockMarketSite/StockMarketApp/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='candlestick_chart')
+    path('index', views.index, name='index'),
+    path('home', views.home, name='home'),
 ]
--- StockMarketSite/StockMarketApp/views.py
+++ StockMarketSite/StockMarketApp/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def index(request):
+    return render(request, "StockMarketApp/index.html", {})
+
+def home(request):
+    if request.method == 'POST':
+    #    stock_name = request.POST.get('stock_search')
+        return redirect("/index")
+    else:
+        return render(request, "StockMarketApp/candlestick_chart.html", {})
+
+def search(request):
     return render(request, "StockMarketApp/candlestick_chart.html", {})
