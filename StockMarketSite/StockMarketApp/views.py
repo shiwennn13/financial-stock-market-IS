0cf854f3565e23fce97f2a4d8b0e5a1ad5f0d547
yichengng <nyicheng12@gmail.com>
2023-08-27 21:37:47 +0800

    graph test

--- StockMarketSite/StockMarketApp/views.py
+++ StockMarketSite/StockMarketApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 import requests
+import plotly.graph_objects as go
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -16,6 +18,7 @@ def home(request):
         else:
             print(time_frame)
             call_api(time_frame, stock_name)
+            chart()
 
         return redirect("/home")
     else:
@@ -23,7 +26,10 @@ def home(request):
 
 def suggest(request, symbol, time):
     call_api(time, symbol)
-    return render(request, "StockMarketApp/candlestick_chart.html", {})
+    fig = chart()
+    config = {'displayModeBar': False}
+    div = fig.to_html(full_html=False, config=config)
+    return render(request, "StockMarketApp/candlestick_chart.html", {'plot_div': div})
 
 def call_api(time,stock):
     url = ''
@@ -44,3 +50,22 @@ def call_api(time,stock):
             print('Invalid Stock Symbol')
         else:
             print(data)
+
+def chart():
+    open_data = [33.0, 33.3, 33.5, 33.0, 34.1]
+    high_data = [33.1, 33.3, 33.6, 33.2, 34.8]
+    low_data = [32.7, 32.7, 32.8, 32.6, 32.8]
+    close_data = [33.0, 32.9, 33.3, 33.1, 33.1]
+    dates = [datetime(year=2013, month=10, day=10),
+             datetime(year=2013, month=11, day=10),
+             datetime(year=2013, month=12, day=10),
+             datetime(year=2014, month=1, day=10),
+             datetime(year=2014, month=2, day=10)]
+
+    fig = go.Figure(data=[go.Candlestick(x=dates,
+                                         open=open_data, high=high_data,
+                                         low=low_data, close=close_data)])
+
+    fig.update_layout(xaxis_rangeslider_visible=False)
+
+    return fig
