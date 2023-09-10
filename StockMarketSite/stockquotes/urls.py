621a519035292331e8ab4a17782bd327acfe59dd
LEE SHI-WEN <TP062634@mail.apu.edu.my>
2023-09-10 16:34:12 +0800

    Install Django testing

---
+++ StockMarketSite/stockquotes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'stockquotes'
+
+urlpatterns = [
+    path('qhome/', views.qhome, name="qhome"),
+    path('add_stock.html', views.add_stock, name="add_stock"),
+    path('delete/<stock_id>', views.delete, name="delete")
+]
