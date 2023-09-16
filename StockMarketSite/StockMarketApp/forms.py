ceb3de68eaeada3afe6217c223bf035e31a9c58a
LEE SHI-WEN <TP062634@mail.apu.edu.my>
2023-09-16 22:56:34 +0800

    code changes in forms.py

--- StockMarketSite/StockMarketApp/forms.py
+++ StockMarketSite/StockMarketApp/forms.py
@@ -7,6 +7,3 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['username','password1', 'password2']
 
-    def __int__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
